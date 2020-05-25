4fc552207b246e4a0d5acf895b014313fe776804
Matthew Jones <stridera@gmail.com>
2020-05-25 00:35:21 -0700

    Update to use robotron

--- robotronenv.py
+++ robotronenv.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
 from collections import deque
 import random
-import atari_py
 import cv2
 import torch
 
+from robotron.robotron import Robotron
+
 
 class Env():
+    GAMEBOX = [116, 309, 608, 974]
+
     def __init__(self, args):
         self.device = args.device
-        self.ale = atari_py.ALEInterface()
-        self.ale.setInt('random_seed', args.seed)
-        self.ale.setInt('max_num_frames_per_episode', args.max_episode_length)
-        self.ale.setFloat('repeat_action_probability', 0)  # Disable sticky actions
-        self.ale.setInt('frame_skip', 0)
-        self.ale.setBool('color_averaging', False)
-        self.ale.loadROM(atari_py.get_game_path(args.game))  # ROM loading must be done after setting options
-        actions = self.ale.getMinimalActionSet()
-        self.actions = dict([i, e] for i, e in zip(range(len(actions)), actions))
+        self.env = Robotron(fps=0)
+        self.actions = 64
         self.lives = 0  # Life counter (used in DeepMind training)
         self.life_termination = False  # Used to check if resetting only from loss of life
         self.window = args.history_length  # Number of frames to concatenate
@@ -25,7 +21,7 @@ class Env():
         self.training = True  # Consistent with model training mode
 
     def _get_state(self):
-        state = cv2.resize(self.ale.getScreenGrayscale(), (84, 84), interpolation=cv2.INTER_LINEAR)
+        state = self.env.get_state()
         return torch.tensor(state, dtype=torch.float32, device=self.device).div_(255)
 
     def _reset_buffer(self):
@@ -33,42 +29,49 @@ class Env():
             self.state_buffer.append(torch.zeros(84, 84, device=self.device))
 
     def reset(self):
+        info = None
+
         if self.life_termination:
             self.life_termination = False  # Reset flag
-            self.ale.act(0)  # Use a no-op after loss of life
+            _, reward, terminal, info = self.env.step(0)
         else:
             # Reset internals
             self._reset_buffer()
-            self.ale.reset_game()
+            self.env.reset()
             # Perform up to 30 random no-ops before starting
             for _ in range(random.randrange(30)):
-                self.ale.act(0)  # Assumes raw action 0 is always no-op
-                if self.ale.game_over():
-                    self.ale.reset_game()
+                _, reward, terminal, info = self.env.step(0)  # Assumes raw action 0 is always no-op
+                if terminal:
+                    self.env.reset()
         # Process and return "initial" state
         observation = self._get_state()
         self.state_buffer.append(observation)
-        self.lives = self.ale.lives()
+        if info is not None:
+            self.lives = info['lives']
         return torch.stack(list(self.state_buffer), 0)
 
     def step(self, action):
         # Repeat action 4 times, max pool over last 2 frames
         frame_buffer = torch.zeros(2, 84, 84, device=self.device)
-        reward, done = 0, False
+        reward, done, info, lives = 0, False, None, 3
         for t in range(4):
-            reward += self.ale.act(self.actions.get(action))
+            move = action // 8
+            shoot = action % 8
+            action = ((move + 1) * 9) + shoot + 1
+            image, new_reward, terminal, info = self.env.step(action)
+            reward += new_reward
+            lives = info['lives']
             if t == 2:
                 frame_buffer[0] = self._get_state()
             elif t == 3:
                 frame_buffer[1] = self._get_state()
-            done = self.ale.game_over()
+            done = terminal or info['level'] > 1
             if done:
                 break
         observation = frame_buffer.max(0)[0]
         self.state_buffer.append(observation)
         # Detect loss of life as terminal in training mode
         if self.training:
-            lives = self.ale.lives()
             if lives < self.lives and lives > 0:  # Lives > 0 for Q*bert
                 self.life_termination = not done  # Only set flag when not truly done
                 done = True
@@ -85,11 +88,10 @@ class Env():
         self.training = False
 
     def action_space(self):
-        return len(self.actions)
+        return self.actions
 
     def render(self):
-        cv2.imshow('screen', self.ale.getScreenRGB()[:, :, ::-1])
-        cv2.waitKey(1)
+        pass
 
     def close(self):
         cv2.destroyAllWindows()
